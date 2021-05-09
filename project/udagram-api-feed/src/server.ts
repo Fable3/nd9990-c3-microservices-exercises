a16d6c86ecfc6b9b0d5d975c03d4fb75f32eb412
Blahut, György <fable3@gmail.com>
2021-05-09 12:35:19 +0200

    api split (feed & user)

---
+++ project/udagram-api-feed/src/server.ts
@@ -0,0 +1,53 @@
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {FeedRouter} from './controllers/v0/feed/routes/feed.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0_FEED_MODELS} from './controllers/v0/model.index';
+
+
+(async () => {
+  try {
+    console.log('authenticate'); // <== to make sure console.log is working and not overrided!
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+  await sequelize.addModels(V0_FEED_MODELS);
+  await sequelize.sync();
+  console.log( `sequelize ok` );
+
+  const app = express();
+  const port = process.env.PORT || 8080;
+
+  app.use(bodyParser.json());
+
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization',
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: config.url,
+  }));
+
+  // todo: remove path when using reverseproxy
+  app.use('/api/v0/feed', FeedRouter);
+
+  // Root URI call
+  /*app.get( '/', async ( req, res ) => {
+    res.send( '/api/v0/' );
+  } );*/
+
+  console.log( `start on ${port}` );
+  // Start the Server
+  app.listen( port, () => {
+    console.log( `server running ${config.url}:${port}` );
+    console.log( `press CTRL+C to stop server` );
+  } );
+})();
